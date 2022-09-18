a7294e4697c81a570e617acd367b7b12d4e3fc91
Kelin <1311740140@qq.com>
2022-09-18 17:09:31 +0800

    .

---
+++ content/number-theory/Min_25.h
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+const int P = 1e9 + 7;
+
+/*
+ * Description: Calculate sum f(x), where f(x) is a  multiplicative function,
+ *   and f(p) = Poly(p), and f(p ^ k) can be calculated quickly.
+ * Time: O(n ^ 0.75 / log n)
+ */
+template <class fun, fun &F>
+struct Min_25 {
+    vector<ll> w, G, pr;
+    ll n; int m, sqrtn;
+    int id(ll x) { return x <= sqrtn ? x : m - n / x; }
+    void init(ll _n) {
+        n = _n, m = 1, sqrtn = sqrt(n) + 1;
+        w.resize(sqrtn * 2), G = w, pr = {};
+        for (ll i = 1; i <= n; ++i, ++m)
+            w[m] = i = n / (n / i), G[m] = F.G(i); // copy G
+        for (int p = 2; p <= sqrtn; ++p)
+            if (G[p] != G[p - 1]) {
+                double ip = 1. / p; pr.push_back(p);
+                for (int i = m - 1, j = id((ll)p * p); i >= j; --i)
+                    G[i] = (G[i] + F.g(p) * (G[p - 1] - G[id(w[i] * ip + 1e-9)] + P)) % P; // copy G
+            }
+        pr.push_back(sqrtn + 1); // prevent array access out of bounds
+    }
+    int S(ll x, int y) {
+        int res = (G[id(x)] - G[pr[y] - 1] + P) % P; // res = sum a_i * (G_i(x) - G_i(pr_y - 1))
+        for (int i = y; pr[i] * pr[i] <= x; ++i)
+            for (ll k = 1, p = pr[i], q = p; q <= x / p; q *= p, ++k)
+                res = (res + F.f(p, k + 1, q * p) + F.f(p, k, q) * S(x / q, i + 1)) % P;
+        return res;
+    }
+    int S(ll x) { return init(x), S(x, 0) + 1; }
+};
+
+// modify  function
+struct fun {
+    ll g(int p) { return p; }                                // f(p) = poly(p) = sum a_i * g_i(p), g_i(p) = p ^ i
+    ll G(ll n) { return n %= P, (n * (n + 1) / 2 - 1) % P; } // G(n) = sum g - g(1)
+    ll f(int p, int k, ll pk) { return k == 1 ? p : 0; }     // f(p ^ k) can be calculated quickly
+} Fun;
+
+Min_25<fun, Fun> m25;
